a6c0c808fe358993b4f6019e56df6f20a2d5450b
N81E1 <102803191+N81E1@users.noreply.github.com>
2025-02-18 20:27:08 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -500,6 +500,24 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        LINE_USER_ID.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
 if __name__ == "__main__":
     app.run()
     main()
